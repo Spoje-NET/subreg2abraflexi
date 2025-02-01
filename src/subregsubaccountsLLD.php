5e5d982b51f7c86f10dd780e5a6e661ea9c06720
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2025-02-01 14:12:49 +0100

    zabbix LLD now return valued instead of keys

--- src/subregsubaccountsLLD.php
+++ src/subregsubaccountsLLD.php
@@ -48,22 +48,22 @@ try {
     if (\array_key_exists('users', $userListData)) {
         foreach ($userListData['users'] as $subregUserData) {
             $lldLine = [
-                '{#SUBREGID}' => $subregUserData['username'].'_id',
-                '{#USERNAME}' => $subregUserData['username'].'_username',
-                '{#NAME}' => $subregUserData['username'].'_name',
-                '{#CREDIT}' => $subregUserData['username'].'_credit',
-                '{#CURRENCY}' => $subregUserData['username'].'_currency',
-                '{#BILLING_NAME}' => $subregUserData['username'].'_billing_name',
-                '{#BILLING_STREET}' => $subregUserData['username'].'_billing_street',
-                '{#BILLING_CITY}' => $subregUserData['username'].'_billing_city',
-                '{#BILLING_PC}' => $subregUserData['username'].'_billing_pc',
-                '{#BILLING_COUNTRY}' => $subregUserData['username'].'_billing_country',
-                '{#COMPANY_ID}' => $subregUserData['username'].'_company_id',
-                '{#COMPANY_VAT}' => $subregUserData['username'].'_company_vat',
-                '{#EMAIL}' => $subregUserData['username'].'_email',
-                '{#PHONE}' => $subregUserData['username'].'_phone',
-                '{#LASTLOGIN}' => $subregUserData['username'].'_last_login',
-                '{#DOMAINS_COUNT}' => $subregUserData['username'].'_domains_count',
+                '{#SUBREGID}' => $subregUserData['id'],
+                '{#USERNAME}' => $subregUserData['username'],
+                '{#NAME}' => $subregUserData['name'],
+                '{#CREDIT}' => $subregUserData['credit'],
+                '{#CURRENCY}' => $subregUserData['currency'],
+                '{#BILLING_NAME}' => $subregUserData['billing_name'],
+                '{#BILLING_STREET}' => $subregUserData['billing_street'],
+                '{#BILLING_CITY}' => $subregUserData['billing_city'],
+                '{#BILLING_PC}' => $subregUserData['billing_pc'],
+                '{#BILLING_COUNTRY}' => $subregUserData['billing_country'],
+                '{#COMPANY_ID}' => $subregUserData['company_id'],
+                '{#COMPANY_VAT}' => $subregUserData['company_vat'],
+                '{#EMAIL}' => $subregUserData['email'],
+                '{#PHONE}' => $subregUserData['phone'],
+                '{#LASTLOGIN}' => $subregUserData['last_login'],
+                '{#DOMAINS_COUNT}' => $subregUserData['domains_count'],
             ];
 
             if ($username) {
@@ -81,7 +81,7 @@ try {
     $report['message'] = $exc->getMessage();
 }
 
-$written = file_put_contents($destination, json_encode($report, \Ease\Shared::cfg('DEBUG') ? \JSON_PRETTY_PRINT : 0));
+$written = file_put_contents($destination, json_encode($report, \Ease\Shared::cfg('DEBUG') ? \JSON_PRETTY_PRINT|\JSON_INVALID_UTF8_IGNORE : 0));
 $subreg->addStatusMessage(sprintf(_('Saving result to %s'), $destination), $written ? 'success' : 'error');
 
 exit($exitcode);
